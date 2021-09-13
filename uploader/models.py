6afb4d8dbb63875452cf7cc9007e100c31ce5329
ricardoveronica <ricardo_veronica.duran@hotmail.com>
2021-09-12 23:38:59 -0500

    updates uploader model

--- uploader/models.py
+++ uploader/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 
 class UploadedFile(models.Model):
+    name = models.CharField(max_length=100, default='file')
     file = models.FileField()
+
+    def __str__(self):
+        return self.name
